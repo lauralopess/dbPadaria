4f39b6f8136169881e277ea78d12f8c91f1f4c9e
lauralopess <lauralopessts@gmail.com>
2023-11-14 11:55:15 -0300

    Nona versão

--- dbPadaria.sql
+++ dbPadaria.sql
@@ -34,10 +34,25 @@ create table produtos (
     foreign key (idFornecedor) references fornecedores(idFornecedor)
 );
 
+describe produtos;
+
+alter table produtos add column validadeProduto date after precoProduto;
+alter table produtos add column pesoProduto decimal(10,2) after estoqueProduto;
+alter table produtos add column ingredientesProduto text after idFornecedor;
+
 insert into produtos (nomeProduto, descricaoProduto, precoProduto, estoqueProduto, categoriaProduto, idFornecedor) values
 ("Torta de Frango c/ Catupiry", "A Torta de Frango com Catupiry é elaborada massa leve, recheio suculento, do jeitinho que você gosta. Experimente e apaixone- se!", "12.00", 20, "salgados", 1);
 insert into produtos (nomeProduto, descricaoProduto, precoProduto, estoqueProduto, categoriaProduto, idFornecedor) values
 ("Baguete", "Baguete tradição francesa au levain. Pão feito com fermentação natural, sem adição de melhoramentos. As farinhas são as verdadeiras 00 italianas, 100% naturais", "5.00", 30, "Pães", 1);
+
+update produtos set pesoProduto = 0.44 where idProduto = 1;
+update produtos set ingredientesProduto = "Farinha de trigo enriquecida com ferro e ácido fólico, margarina, ovo integral pasteurizado, creme de leite, creme branco 
+(composto lácteo, amido de milho, requeijão cremoso, cloreto de sódio (sal)), requeijão cremoso, frango desfiado, cebola, alho, salsinha, molho de tomate e colorau.
+ALÉRGICOS: CONTÉM DERIVADOS DE TRIGO, SOJA, OVO E LEITE. PODE CONTER NOZES E PEIXES. CONTÉM LACTOSE. CONTÉM GLÚTEN" where idProduto = 1;
+update produtos set validadeProduto = "2023-11-25" where idProduto = 1;
+update produtos set pesoProduto = 0.2 where idProduto = 2;
+update produtos set ingredientesProduto = "farinha de aveia, fermento seco, água, sal" where idProduto = 2;
+update produtos set validadeProduto = "2023-11-22" where idProduto = 2;
 
 select * from produtos;
 select * from produtos where categoriaProduto = "Pães";
@@ -92,3 +107,11 @@ select nomeCliente, pedidos.idPedido, dataPedido, produtos.idProduto, nomeProdut
 from itensPedidos inner join pedidos on itensPedidos.idPedido = pedidos.idPedido
 inner join produtos on itensPedidos.idProduto = produtos.idProduto
 inner join clientes on pedidos.idCliente = clientes.idCliente;
+
+select sum(quantidade * precoProduto) as Total from produtos inner join itensPedidos on produtos.idProduto = itensPedidos.idProduto where idPedido = 1;
+
+select * from produtos where validadeProduto > curdate();
+
+select * from produtos where ingredientesProduto like '%agua%';
+
+select * from produtos where ingredientesProduto not like '%trigo%' and precoProduto <= 7.90;
